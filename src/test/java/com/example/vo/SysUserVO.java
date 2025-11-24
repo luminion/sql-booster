5d6ee5a2ad9d71395e256b19cc484d81e1d4fcef
lo <604647004@qq.com>
2025-11-24 14:52:17 +0800

    refactor(test): 移除SysUserVO中的Excel注解和冗余字段
    - 删除了Excel相关的注解引入
    - 移除了类上的@ExcelIgnoreUnannotated注解
    - 清理了所有字段上的@ExcelProperty注解
    - 去除了nameLike字段
    - 简化了VO类结构，仅保留基础数据字段

--- src/test/java/com/example/vo/SysUserVO.java
+++ src/test/java/com/example/vo/SysUserVO.java
@@ -1,7 +1,5 @@
 package com.example.vo;
 
-import cn.idev.excel.annotation.ExcelIgnoreUnannotated;
-import cn.idev.excel.annotation.ExcelProperty;
 import com.example.entity.SysUser;
 import com.example.mapper.SysUserMapper;
 import lombok.Data;
@@ -18,63 +16,52 @@ import java.time.LocalDateTime;
  * @see SysUserMapper
  */
 @Data
-@ExcelIgnoreUnannotated
 public class SysUserVO {
 
     /**
      * id
      */
-//    @ExcelProperty(value = "id")
     private Long id;
 
     /**
      * 版本号
      */
-    @ExcelProperty(value = "版本号")
     private Integer version;
 
     /**
      * 创建时间
      */
-    @ExcelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
-    @ExcelProperty(value = "更新时间")
     private LocalDateTime updateTime;
 
     /**
      * 姓名
      */
-    @ExcelProperty(value = "姓名")
     private String name;
     
-    @ExcelProperty(value = "姓名Like后缀")
     private String nameLike;
 
     /**
      * 描述
      */
-    @ExcelProperty(value = "描述")
     private String description;
 
     /**
      * 年龄
      */
-    @ExcelProperty(value = "年龄")
     private Integer age;
 
     /**
      * 生日
      */
-    @ExcelProperty(value = "生日")
     private LocalDate birthDate;
 
     /**
      * 状态
      */
-    @ExcelProperty(value = "状态")
     private Integer state;
 }
